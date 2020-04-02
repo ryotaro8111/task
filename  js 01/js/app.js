4219bca5894f163f16dd32242720465876cc2e80
ryotaro8111 <ryo8111@kinoshitaryoutarounoMacBook-ea.local>
2020-04-02 18:25:20 +0900

    計算ツールの編集

---  js 01/js/app.js
+++  js 01/js/app.js
@@ -2,5 +2,7 @@
 let price = window.prompt("合計金額を入力して下さい。");
 let name =window.prompt("ユーザー名を入力してください。");
 const tax =1.10;
+let totalPrice = price*tax ;
+let priceAfter = Math.floor(totalPrice);
 
-alert(`${name}さんの合計金額は${price*tax}円です`)
+alert(`${name}さんの合計金額は${priceAfter}円です`)
